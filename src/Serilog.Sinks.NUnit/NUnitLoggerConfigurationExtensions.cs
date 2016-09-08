01d312a19e024be2c5b69d4439b96dc121c86379
Lukas Behal <lbehal@TradeMe.local>
2016-09-08 17:48:16 +1200

    Initial commit

---
+++ src/Serilog.Sinks.NUnit/NUnitLoggerConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.NUnit.Sinks.NUnit;
+
+namespace Serilog.Sinks.NUnit
+{
+    public static class NUnitLoggerConfigurationExtensions
+    {
+        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}  {Exception}";
+
+        public static LoggerConfiguration NUnitOutput(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            IFormatProvider formatProvider = null,
+            LoggingLevelSwitch levelSwitch = null,
+            string outputTemplate = DefaultOutputTemplate)
+        {
+            if (sinkConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(sinkConfiguration));
+            }
+
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+
+            return sinkConfiguration.Sink(new NUnitSink(formatter), restrictedToMinimumLevel, levelSwitch);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.NUnit/Sinks/NUnit/NUnitSink.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+
+namespace Serilog.Sinks.NUnit.Sinks.NUnit
+{
+    public class NUnitSink : ILogEventSink
+    {
+        private readonly MessageTemplateTextFormatter _formatter;
+
+        public NUnitSink(MessageTemplateTextFormatter formatter)
+        {
+            if (formatter == null)
+            {
+                throw new ArgumentNullException(nameof(formatter));
+            }
+
+            _formatter = formatter;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null)
+            {
+                throw new ArgumentNullException(nameof(logEvent));
+            }
+
+            if (TestContext.Out != null)
+            {
+                var writer = new StringWriter();
+                _formatter.Format(logEvent, writer);
+
+                TestContext.Out.WriteLine(writer.ToString());
+            }
+        }
+    }
+}
