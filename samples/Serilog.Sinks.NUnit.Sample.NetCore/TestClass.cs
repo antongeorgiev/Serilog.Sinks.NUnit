b88903291f9bf458ab683dfe34149d59d6eb2053
luk355 <luk355@gmail.com>
2016-09-13 18:21:40 +1200

    Added sample projects

---
+++ samples/Serilog.Sinks.NUnit.Sample.NetCore/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Serilog.Sinks.NUnit.Sample.NetCore
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.NUnitOutput()
+                .CreateLogger();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Log.Error("Blahblahblah!");
+        }
+    }
+}
